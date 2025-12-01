9c692941b09d6286d1ff32ca70212113ffd32650
Andrej Zdvořák <andrej.zdvorak.123@gmail.com>
2025-12-01 19:54:39 +0100

    song search field ui

--- client/app/(tabs)/songs.tsx
+++ client/app/(tabs)/songs.tsx
@@ -1,19 +1,27 @@
-import PageContainer from "@/components/PageContainer";
-import { View, Text, ScrollView, Pressable } from "react-native";
 import NoBand from "@/components/NoBand";
+import PageContainer from "@/components/PageContainer";
+import StyledButton from "@/components/StyledButton";
+import StyledTextInput from "@/components/StyledTextInput";
+import SwitchTabs from "@/components/SwitchTabs";
 import { useAuth } from "@/context/AuthContext";
 import { useBand } from "@/context/BandContext";
 import { useAccessibleFontSize } from "@/hooks/use-accessible-font-size";
-import SwitchTabs from "@/components/SwitchTabs";
+import {
+    Calendar,
+    Clock,
+    EllipsisVertical,
+    Hash,
+    Play,
+    SquarePen,
+} from "lucide-react-native";
 import { useState } from "react";
-import StyledButton from "@/components/StyledButton";
-import { Play } from "lucide-react-native";
-import { SquarePen } from "lucide-react-native";
-import { Calendar } from "lucide-react-native";
-import { Clock } from "lucide-react-native";
-import { Hash } from "lucide-react-native";
-import { EllipsisVertical } from "lucide-react-native";
-import { useColorScheme } from "react-native";
+import {
+    Pressable,
+    ScrollView,
+    Text,
+    useColorScheme,
+    View,
+} from "react-native";
 
 const songs = () => {
     const { user } = useAuth();
@@ -39,90 +47,100 @@ const songs = () => {
         description: string;
     }) => {
         return (
-            <View className='bg-boxBackground-light dark:bg-boxBackground-dark border border-accent-light dark:border-accent-dark rounded-2xl p-5 w-full mb-3'>
+            <View className="bg-boxBackground-light dark:bg-boxBackground-dark border border-accent-light dark:border-accent-dark rounded-2xl p-5 w-full mb-3">
                 <View
-                    className='flex-row justify-between items-center'
-                    style={{ flexWrap: "wrap" }}>
+                    className="flex-row justify-between items-center"
+                    style={{ flexWrap: "wrap" }}
+                >
                     <View
-                        className='flex-col'
-                        style={{ flexShrink: 1, flex: 1, minWidth: 0 }}>
+                        className="flex-col"
+                        style={{ flexShrink: 1, flex: 1, minWidth: 0 }}
+                    >
                         <View
-                            className='flex-row items-center gap-2'
-                            style={{ flexWrap: "wrap" }}>
+                            className="flex-row items-center gap-2"
+                            style={{ flexWrap: "wrap" }}
+                        >
                             <Text
-                                className='font-bold text-black dark:text-white'
+                                className="font-bold text-black dark:text-white"
                                 style={{ fontSize: fontSize.xl }}
                                 numberOfLines={1}
-                                maxFontSizeMultiplier={1.3}>
+                                maxFontSizeMultiplier={1.3}
+                            >
                                 {songName}
                             </Text>
                             <Text
                                 className={`${status === "ready" ? "text-green bg-transparentGreen" : status === "draft" ? "text-violet bg-transparentViolet" : status === "finished" && "text-blue bg-transparentBlue"} my-1 px-3 py-1 rounded-xl mr-2`}
                                 style={{ fontSize: fontSize.base }}
                                 numberOfLines={1}
-                                maxFontSizeMultiplier={1.3}>
+                                maxFontSizeMultiplier={1.3}
+                            >
                                 {status}
                             </Text>
                         </View>
                         <View
-                            className='flex-row gap-2'
-                            style={{ flexWrap: "wrap" }}>
-                            <View className='flex-row items-center gap-1'>
+                            className="flex-row gap-2"
+                            style={{ flexWrap: "wrap" }}
+                        >
+                            <View className="flex-row items-center gap-1">
                                 <Clock
                                     color={"#A1A1A1"}
                                     size={Math.min(fontSize["2xl"], 20)}
                                     style={{ marginRight: 2, marginBottom: -2 }}
                                 />
                                 <Text
-                                    className='text-silverText'
+                                    className="text-silverText"
                                     style={{
                                         fontSize: fontSize.base,
                                         alignItems: "center",
                                     }}
                                     numberOfLines={1}
-                                    maxFontSizeMultiplier={1.3}>
+                                    maxFontSizeMultiplier={1.3}
+                                >
                                     {length}
                                 </Text>
                             </View>
-                            <View className='flex-row items-center gap-1'>
+                            <View className="flex-row items-center gap-1">
                                 <Hash
                                     color={"#A1A1A1"}
                                     size={Math.min(fontSize["2xl"], 20)}
                                     style={{ marginRight: 2, marginBottom: -2 }}
                                 />
                                 <Text
-                                    className='text-silverText'
+                                    className="text-silverText"
                                     style={{
                                         fontSize: fontSize.base,
                                         alignItems: "center",
                                     }}
                                     numberOfLines={1}
-                                    maxFontSizeMultiplier={1.3}>
+                                    maxFontSizeMultiplier={1.3}
+                                >
                                     {songKey}
                                 </Text>
                             </View>
-                            <View className='flex-row items-center gap-1'>
+                            <View className="flex-row items-center gap-1">
                                 <Calendar
                                     color={"#A1A1A1"}
                                     size={Math.min(fontSize["2xl"], 20)}
                                     style={{ marginRight: 2, marginBottom: -2 }}
                                 />
                                 <Text
-                                    className='text-silverText'
+                                    className="text-silverText"
                                     style={{
                                         fontSize: fontSize.base,
                                         alignItems: "center",
                                     }}
                                     numberOfLines={1}
-                                    maxFontSizeMultiplier={1.3}>
+                                    maxFontSizeMultiplier={1.3}
+                                >
                                     {dateAdded}
                                 </Text>
                             </View>
                         </View>
                     </View>
                     <View
-                        className='flex-row gap-4 items-center'
-                        style={{ flexShrink: 0 }}>
+                        className="flex-row gap-4 items-center"
+                        style={{ flexShrink: 0 }}
+                    >
                         <Play
                             color={colorScheme === "dark" ? "white" : "black"}
                             size={20}
@@ -146,10 +164,11 @@ const songs = () => {
                     </View>
                 </View>
                 <Text
-                    className='text-silverText my-2'
+                    className="text-silverText my-2"
                     style={{ fontSize: fontSize.base }}
                     numberOfLines={3}
-                    maxFontSizeMultiplier={1.3}>
+                    maxFontSizeMultiplier={1.3}
+                >
                     {description}
                 </Text>
             </View>
@@ -162,21 +181,23 @@ const songs = () => {
                 <NoBand />
             ) : (
                 <>
-                    <View className='flex-row justify-between items-start w-full border-b border-accent-light dark:border-accent-dark mt-4 w-full px-3 py-2'>
-                        <View className='flex-col items-start justify-center'>
+                    <View className="flex-row justify-between items-start w-full border-b border-accent-light dark:border-accent-dark mt-4 w-full px-3 py-2">
+                        <View className="flex-col items-start justify-center">
                             <Text
-                                className='text-black dark:text-white font-bold my-1'
-                                style={{ fontSize: fontSize["2xl"] }}>
+                                className="text-black dark:text-white font-bold my-1"
+                                style={{ fontSize: fontSize["2xl"] }}
+                            >
                                 {activeBand?.name} Songs & Setlists
                             </Text>
                             <Text
-                                className='text-silverText'
-                                style={{ fontSize: fontSize.base }}>
+                                className="text-silverText"
+                                style={{ fontSize: fontSize.base }}
+                            >
                                 Manage your band's songs and setlists
                             </Text>
                         </View>
                     </View>
-                    <View className='flex-row justify-between items-start w-full border-b border-accent-light dark:border-accent-dark w-full px-3'>
+                    <View className="flex-row justify-between items-start w-full border-b border-accent-light dark:border-accent-dark w-full px-3">
                         <SwitchTabs
                             activeTab={activeTab}
                             setActiveTab={setActiveTab}
@@ -185,55 +206,65 @@ const songs = () => {
                     </View>
                     {activeTab === "Songs" ? (
                         <>
-                            <View className='flex-row justify-between items-center w-full border-b border-accent-light dark:border-accent-dark w-full px-5 py-3'>
-                                <Text
-                                    className='text-silverText'
-                                    style={{ fontSize: fontSize.base }}>
-                                    4 songs • 2 ready
-                                </Text>
-                                <StyledButton
-                                    onPress={() => {}}
-                                    title='+  New Song'
-                                />
+                            <View className="flex-col justify-center items-center gap-3 w-full border-b border-accent-light dark:border-accent-dark w-full px-5 py-3">
+                                <View className="flex-row justify-between items-center w-full">
+                                    <Text
+                                        className="text-silverText"
+                                        style={{ fontSize: fontSize.base }}
+                                    >
+                                        4 songs • 2 ready
+                                    </Text>
+                                    <StyledButton
+                                        onPress={() => {}}
+                                        title="+  New Song"
+                                    />
+                                </View>
+                                <View className="flex-row justify-between items-center w-full">
+                                    <StyledTextInput
+                                        placeholder="Search songs"
+                                        variant="rounded"
+                                    />
+                                </View>
                             </View>
                             <ScrollView
-                                className='flex-col px-3 py-4 w-full'
+                                className="flex-col px-3 py-4 w-full"
                                 contentContainerStyle={{
                                     alignItems: "center",
                                     justifyContent: "center",
                                     paddingBottom: 25,
-                                }}>
+                                }}
+                            >
                                 <SongCard
-                                    songName='Song Name'
-                                    status='ready'
-                                    dateAdded='dateAdded'
-                                    description='This is a very long description that will test the expansitivity of the song card and also its legibility'
-                                    songKey='Am'
-                                    length='3:33'
+                                    songName="Song Name"
+                                    status="ready"
+                                    dateAdded="dateAdded"
+                                    description="This is a very long description that will test the expansitivity of the song card and also its legibility"
+                                    songKey="Am"
+                                    length="3:33"
                                 />
                                 <SongCard
-                                    songName='Song Name'
-                                    status='finished'
-                                    dateAdded='dateAdded'
-                                    description='desc'
-                                    songKey='Am'
-                                    length='3:33'
+                                    songName="Song Name"
+                                    status="finished"
+                                    dateAdded="dateAdded"
+                                    description="desc"
+                                    songKey="Am"
+                                    length="3:33"
                                 />
                                 <SongCard
-                                    songName='Song Name'
-                                    status='draft'
-                                    dateAdded='dateAdded'
-                                    description='desc'
-                                    songKey='Am'
-                                    length='3:33'
+                                    songName="Song Name"
+                                    status="draft"
+                                    dateAdded="dateAdded"
+                                    description="desc"
+                                    songKey="Am"
+                                    length="3:33"
                                 />
                                 <SongCard
-                                    songName='Song Name'
-                                    status='ready'
-                                    dateAdded='dateAdded'
-                                    description='desc'
-                                    songKey='Am'
-                                    length='3:33'
+                                    songName="Song Name"
+                                    status="ready"
+                                    dateAdded="dateAdded"
+                                    description="desc"
+                                    songKey="Am"
+                                    length="3:33"
                                 />
                             </ScrollView>
                         </>
