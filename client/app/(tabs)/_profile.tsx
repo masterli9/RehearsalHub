f80eb93fd26d52ee9e14f6356492af1447d386fb
Andrej Zdvořák <andrej.zdvorak.123@gmail.com>
2025-12-08 21:48:07 +0100

    first profile page draft

--- client/app/(tabs)/_profile.tsx
+++ client/app/(tabs)/_profile.tsx
@@ -1,17 +1,19 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import { useBand } from "@/context/BandContext";
 import { useAuth } from "@/context/AuthContext";
 import { useAccessibleFontSize } from "@/hooks/use-accessible-font-size";
+import { LogOut, SquarePen } from "lucide-react-native";
+import { useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const profile = () => {
     const { activeBand } = useBand();
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
     const fontSize = useAccessibleFontSize();
-
-    console.log(user?.photoURL);
+    const colorScheme = useColorScheme();
 
     return (
-        <>
+        <SafeAreaView className='flex-1'>
             <View className='flex-row justify-between items-start w-full border-b border-accent-light dark:border-accent-dark mt-4 w-full px-5 py-2'>
                 <View className='flex-col items-start justify-center'>
                     <Text
@@ -26,25 +28,76 @@ const profile = () => {
                     </Text>
                 </View>
             </View>
-            <View className='flex-row items-center'>
-                <Image
-                    source={{ uri: user?.photoURL || "" }}
-                    className='w-12 h-12 rounded-full'
-                />
-                <View className='flex-col'>
-                    <Text
-                        className='text-silverText'
-                        style={{ fontSize: fontSize.base }}>
-                        {user?.username}
+            <View className='flex-1 items-center my-5 px-5'>
+                <View className='flex-col bg-darkWhite dark:bg-darkGray rounded rounded-xl w-full'>
+                    <View className='flex-row items-center px-3 gap-3 border-b border-accent-light dark:border-accent-dark py-3'>
+                        <Image
+                            source={{ uri: user?.photoURL || "" }}
+                            className='w-12 h-12 rounded-full'
+                        />
+                        <View className='flex-col'>
+                            <Text
+                                className='text-black dark:text-white font-bold'
+                                style={{ fontSize: fontSize["2xl"] }}>
+                                {user?.username}
+                            </Text>
+                            <Text
+                                className='text-silverText'
+                                style={{ fontSize: fontSize.base }}>
+                                {user?.email}
+                            </Text>
+                        </View>
+                    </View>
+                    <Pressable className='px-3 py-5 flex-row items-center gap-3 w-full border-b border-accent-light dark:border-accent-dark'>
+                        <SquarePen
+                            size={Math.min(fontSize["3xl"], 20)}
+                            style={{
+                                marginRight: 2,
+                                marginBottom: -2,
+                            }}
+                            color={colorScheme === "dark" ? "#fff" : "#000"}
+                        />
+                        <Text
+                            className='text-black dark:text-white'
+                            style={{ fontSize: fontSize.base }}>
+                            Edit profile
+                        </Text>
+                    </Pressable>
+                    <Pressable
+                        className='px-3 py-5 flex-row items-center gap-3 w-full'
+                        onPress={() => logout()}>
+                        <LogOut
+                            size={Math.min(fontSize["3xl"], 20)}
+                            style={{
+                                marginRight: 2,
+                                marginBottom: -2,
+                            }}
+                            color={colorScheme === "dark" ? "#fff" : "#000"}
+                        />
+                        <Text
+                            className='text-black dark:text-white'
+                            style={{ fontSize: fontSize.base }}>
+                            Log Out
+                        </Text>
+                    </Pressable>
+                </View>
+                <View>
+                    <Text className='font-bold text-black dark:text-white'>
+                        Appearance
                     </Text>
-                    <Text
-                        className='text-silverText'
-                        style={{ fontSize: fontSize.base }}>
-                        {user?.email}
+                </View>
+                <View>
+                    <Text className='font-bold text-black dark:text-white'>
+                        Switch bands
+                    </Text>
+                </View>
+                <View>
+                    <Text className='font-bold text-black dark:text-white'>
+                        Quick settings
                     </Text>
                 </View>
             </View>
-        </>
+        </SafeAreaView>
     );
 };
 
