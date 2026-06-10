0a75dd247da81dbe8bf4740f876fee47d1291065
Andrej Zdvořák <andrej.zdvorak.123@gmail.com>
2026-06-10 22:00:44 +0200

    ssl/tls

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -9,6 +9,11 @@ const pool = new pg.Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl:
+        process.env.DB_HOST === "localhost" ||
+        process.env.DB_HOST === "127.0.0.1"
+            ? false
+            : { rejectUnauthorized: false },
 });
 
 export default pool;
